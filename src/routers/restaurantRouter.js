ad0eda8edee27d695e0c1b3ad5502fef602adefb
Cherilyn Eugenia <cherilyn.eugenia@gmail.com>
2023-06-01 21:30:15 +0700

    add middleware to routes

--- src/routers/restaurantRouter.js
+++ src/routers/restaurantRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const RestaurantReservationController = require("../controllers/RestaurantReservationController");
+const { RestaurantMiddleware } = require("../middlewares/RoleMiddleware");
 
 const restaurantRouter = Router();
 
-restaurantRouter.get("/reservation",RestaurantReservationController.getAllReservation)
-restaurantRouter.get("/reservation/:reservation_id",RestaurantReservationController.getReservationById)
+restaurantRouter.get("/reservation", RestaurantMiddleware, RestaurantReservationController.getAllReservation)
+restaurantRouter.get("/reservation/:reservation_id", RestaurantMiddleware, RestaurantReservationController.getReservationById)
 
 module.exports = restaurantRouter;
--- src/routers/seekerRouter.js
+++ src/routers/seekerRouter.js
@@ -1,16 +1,17 @@
 const { Router } = require("express");
 const SeekerController = require("../controllers/SeekerController");
 const SeekerReservationController = require("../controllers/SeekerReservationController");
+const { SeekerMiddleware } = require("../middlewares/RoleMiddleware");
 
 const seekerRouter = Router();
 
-seekerRouter.get("/",SeekerController.getRestaurant)
+seekerRouter.get("/", SeekerMiddleware, SeekerController.getRestaurant)
 
 //reservations
-seekerRouter.post('/reservation', SeekerReservationController.addReservation)
-seekerRouter.put('/reservation/:reservation_id', SeekerReservationController.rescheduleReservation)
-seekerRouter.delete('/reservation/:reservation_id', SeekerReservationController.cancelReservation)
-seekerRouter.get('/reservation', SeekerReservationController.getHistoryReservation)
-seekerRouter.get('/reservation/:reservation_id', SeekerReservationController.getReservationById)
+seekerRouter.post('/reservation', SeekerMiddleware, SeekerReservationController.addReservation)
+seekerRouter.put('/reservation/:reservation_id', SeekerMiddleware, SeekerReservationController.rescheduleReservation)
+seekerRouter.delete('/reservation/:reservation_id', SeekerMiddleware, SeekerReservationController.cancelReservation)
+seekerRouter.get('/reservation', SeekerMiddleware, SeekerReservationController.getHistoryReservation)
+seekerRouter.get('/reservation/:reservation_id', SeekerMiddleware, SeekerReservationController.getReservationById)
 
 module.exports = seekerRouter;
