6d9ac639e590147dca5aac07380261e34c9cfa45
CBEngineer <CBEalexanderkevin@gmail.com>
2023-06-20 14:28:45 +0700

    fix migration case inconsistency

--- src/database/migrations/20230504051900-create-user.js
+++ src/database/migrations/20230504051900-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       user_id: {
         allowNull: false,
         primaryKey: true,
